6134b693b0e0885f1b82be8879591103ac12cfc1
rdiazcn <143457670+rdiazcn@users.noreply.github.com>
2023-12-04 20:14:34 -0500

    Delete Proyecto Final directory

--- Proyecto Final/Modern_Exteriors_Complete_Tileset_48x48.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Modern_Exteriors_Complete_Tileset_48x48" tilewidth="48" tileheight="48" tilecount="49808" columns="176">
- <image source="../../Downloads/modernexteriors-win/Modern_Exteriors_48x48/Modern_Exteriors_Complete_Tileset_48x48.png" width="8448" height="13584"/>
-</tileset>
